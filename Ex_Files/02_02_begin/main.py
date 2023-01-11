ee886bf7ae73d300ec3479e18a811345b1f2c7f6
Candace3308 <122392428+Candace3308@users.noreply.github.com>
2023-01-11 17:50:50 +0000

    testing

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -1,12 +1,32 @@
 greet = "Hello World"
 extened_grt = "Hello World, " + "this is a long string"
-
-name = "John"
-
-intrupution = f"Hello {name}"
-
 greet_format = "Hello {}"
-
+name = "Daniel"
+intrupution = f"Hello {name}"
 formatted = greet_format.format(name)
 
-print(intrupution, formatted)
+#print(intrupution)
+#print(formatted)
+#print(formatted.upper())
+#print(formatted.lower())
+#print(formatted.replace("Daniel", "Candace"))
+#print(greet, formatted)
+#print(greet, formatted.replace("Daniel", "Candace"))
+#print(extened_grt)
+
+hello = "testing"
+world = "this"
+number = "because"
+thanks = "why"
+goodbye = "not"
+salary = "77000"
+
+hi = f"{hello} {world}"
+print(hi)
+
+hstring = "{} {} {} {} {}".format(hello, world, number, thanks, goodbye)
+print(hstring)
+
+fsalary = f"{name}'s ${salary:.2f}"
+print(fsalary)
+
